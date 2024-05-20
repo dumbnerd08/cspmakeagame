965f1e66706402c40a4e09c89145245ad01b267c
Teo <teo.sivanich08@gmail.com>
2024-05-20 12:20:27 -0500

    add stuff

--- files/index.js
+++ files/index.js
@@ -1,10 +1,23 @@
-const express=require("express")
-const app=express();
+const express = require("express")
+const app = express();
+const path = require('path');
 
-app.get("/space.hdr",(req,res,next)=>{
-	res.send("test");
-})
+app.get("/space.hdr", function (req, res) {
+	const options = {
+		root: path.join(__dirname)
+	};
 
-app.listen(8000,()=>{
-	console.log("running");
-})
+	const fileName = "space.hdr";
+	res.sendFile(fileName, options, function (err) {
+		if (err) {
+			console.log("error");
+		} else {
+			console.log("sent");
+		}
+	});
+});
+
+app.listen(8080, function (err) {
+	if (err) console.error(err);
+	console.log("listening on port 8000");
+});
