2cdcfa8b18a5cdcd4a0835aa604e80726a66f08d
Teo <teo.sivanich08@gmail.com>
2024-05-24 11:34:19 -0500

    did stuff

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,10 @@ pub async fn run() {
     let context = window.gl();
     let mut camera = Camera::new_perspective(
         window.viewport(),
-        vec3(2800.0, 240.0, 1700.0),
+        vec3(-600.0, 600.0, 600.0),
         vec3(0.0, 0.0, 0.0),
         vec3(0.0, 1.0, 0.0),
-        degrees(60.0),
+        degrees(45.0),
         0.1,
         10000.0,
     );
@@ -30,17 +30,29 @@ pub async fn run() {
     );
 
     let mut loaded = if let Ok(loaded) = three_d_asset::io::load_async(&[
-        "/home/teo/cspmakeagame/assets/space.hdr"]).await
+        "/home/teo/cspmakeagame/assets/space.hdr", "/home/teo/cspmakeagame/assets/iss.obj"]).await
     {
         loaded
     } else {
         println!("loading from web");
         three_d_asset::io::load_async(&[
-            "http://localhost/space.hdr",])
+            "http://localhost/space.hdr",
+            "http://localhost/iss.obj",])
             .await
             .expect("this is an error message")
     };
-
+    
+    let cpu_model: CpuModel = loaded.deserialize("iss.obj").unwrap();
+    let mut model = Vec::new();
+    let scale = Mat4::from_scale(10.0);
+    
+    let mut iss = Model::<PhysicalMaterial>::new(&context,
+        &loaded.deserialize("iss.obj").unwrap())
+            .unwrap();
+    iss.iter_mut().for_each(|m| {
+        m.material.render_states.cull = Cull::Back;
+        m.set_transformation(Mat4::from_angle_x(degrees(-90.0)));
+    });
     let skybox = Skybox::new_from_equirectangular(&context, &loaded.deserialize("hdr").unwrap());
     let light = AmbientLight::new_with_environment(&context, 0.7, Srgba::WHITE, skybox.texture());
     let mut gui = GUI::new(&context);
@@ -52,13 +64,7 @@ pub async fn run() {
             frame_input.accumulated_time,
             frame_input.viewport,
             frame_input.device_pixel_ratio,
-            |gui_context| {
-                
-                /*SidePanel::left("side_panel").show(gui_context, |ui| {
-                    ui.heading("FLAPPY BIRD");
-                });
-                panel_width = gui_context.used_rect().width();*/
-            },
+            |gui_context| {},
         );
         let viewport = Viewport {
             x: (panel_width * frame_input.device_pixel_ratio) as i32,
@@ -73,8 +79,19 @@ pub async fn run() {
             .screen()
             .clear(ClearState::color_and_depth(0.5, 0.5, 0.5, 1.0, 1.0))
             .render(&camera, skybox.into_iter(), &[&light])
-            .write(|| gui.render())
+            .write(|| {
+                for object in model
+                    .iter()
+                    .flatten()
+                    .chain(&iss)
+                    .filter(|o| camera.in_frustum(&o.aabb()))
+                {
+                    object.render(&camera, &[&light]);
+                }
+                gui.render()
+            })
             .unwrap();
+
         FrameOutput::default()
     });
 }
