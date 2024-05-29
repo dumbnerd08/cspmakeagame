f765cccc21584ef27d1926303daafc616e0a71e8
Teo <teo.sivanich08@gmail.com>
2024-05-29 18:24:02 -0500

    add fixes

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,3 +1,4 @@
-./deleteimages.sh
+./deleteimage.sh
+./deletecontainer.sh
 npx wasm-pack build "./" --target web --out-name web --out-dir ./web/pkg
 yes | docker compose rm && docker compose build && docker compose up
